f9e7300fe5a5ebf32ec8b713ea146cb2606bd893
Brunowcst <brunoporfirio022@gmail.com>
2024-02-06 02:55:00 -0300

    initial commit: add initial files

---
+++ src/main/java/com/rocketseat/project_nwl/ProjectNwlApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.project_nwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNwlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/project_nwl/ProjectNwlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.project_nwl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNwlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
