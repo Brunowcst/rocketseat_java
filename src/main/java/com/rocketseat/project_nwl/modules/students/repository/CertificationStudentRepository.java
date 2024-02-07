a63738995b8e42e2ccc5d7b1101c8618d0227cca
Brunowcst <brunoporfirio022@gmail.com>
2024-02-07 00:34:07 -0300

    add: create certification student repository

---
+++ src/main/java/com/rocketseat/project_nwl/modules/students/repository/CertificationStudentRepository.java
@@ -0,0 +1,16 @@
+package com.rocketseat.project_nwl.modules.students.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.rocketseat.project_nwl.modules.students.entities.CertificationStudentEntity;
+
+@Repository
+public interface CertificationStudentRepository extends JpaRepository<CertificationStudentEntity, UUID> {
+    @Query("SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
+    List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
+} 
