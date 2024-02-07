00366c6014200b5fa34307776d206765ad0f7923
Brunowcst <brunoporfirio022@gmail.com>
2024-02-06 23:47:42 -0300

    add: update model Answers Certifications

--- src/main/java/com/rocketseat/project_nwl/modules/students/entities/AnswersCertificationsEntity.java
+++ src/main/java/com/rocketseat/project_nwl/modules/students/entities/AnswersCertificationsEntity.java
@@ -1,7 +1,17 @@
 package com.rocketseat.project_nwl.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +19,36 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "answers_certification_students")
 public class AnswersCertificationsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID certification_id;
-    private UUID student_id;
-    private UUID question_id;
-    private UUID answer_id;
+
+    @Column(name = "certification_id")
+    private UUID certificationID;
+
+    @ManyToOne
+    @JoinColumn(name = "certification_id", insertable = false, updatable = false)
+    private CertificationStudentEntity certificationStudentEntity;
+
+    @Column(name = "student_id")
+    private UUID studentID;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @Column(name = "question_id")
+    private UUID questionId;
+
+    @Column(name = "answer_id")
+    private UUID answerId;
+
+    @Column(name = "is_correct")
     private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 }
