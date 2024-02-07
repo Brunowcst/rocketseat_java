6703c64f1850735358e3679409935ccef5b4830b
Brunowcst <brunoporfirio022@gmail.com>
2024-02-07 00:34:28 -0300

    add: update verify has certification

--- src/main/java/com/rocketseat/project_nwl/modules/students/useCases/VerifyIsHasCertificationUseCase.java
+++ src/main/java/com/rocketseat/project_nwl/modules/students/useCases/VerifyIsHasCertificationUseCase.java
@@ -1,13 +1,21 @@
 package com.rocketseat.project_nwl.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rocketseat.project_nwl.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.project_nwl.modules.students.repository.CertificationStudentRepository;
 
 @Service
 public class VerifyIsHasCertificationUseCase {
+
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyHasCertificationDTO dto) {
-        if (dto.getEmail().equals("teste@gmail.com") && dto.getTechnology().equals("Java")) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        
+        if (!result.isEmpty()) {
             return true;
         } 
         return false;
