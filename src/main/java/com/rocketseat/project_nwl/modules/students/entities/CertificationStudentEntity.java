ae430ee7db24a2bbc0990c358b74cd49357a8288
Brunowcst <brunoporfirio022@gmail.com>
2024-02-08 17:51:18 -0300

    add: update certification student entity

--- src/main/java/com/rocketseat/project_nwl/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/rocketseat/project_nwl/modules/students/entities/CertificationStudentEntity.java
@@ -6,6 +6,10 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,12 +19,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity(name = "certifications")
 public class CertificationStudentEntity {
 
@@ -34,15 +40,17 @@ public class CertificationStudentEntity {
     @Column(length = 10)
     private int grade;
 
-    @JoinColumn(name = "student_id")
+    @Column(name = "student_id")
     private UUID student_id;
 
     @ManyToOne
     @JoinColumn(name = "student_id", insertable = false , updatable = false)
+    @JsonBackReference
     private StudentEntity studentEntity;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "answer_certification_id", insertable = false , updatable = false)
+    @JsonManagedReference
     private List<AnswersCertificationsEntity> answersCertificationsEntities; 
 
     @CreationTimestamp
