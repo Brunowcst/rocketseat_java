e01db86d9c624552f9ddfed6d2296da7b0c7a9ed
Brunowcst <brunoporfirio022@gmail.com>
2024-02-08 17:51:30 -0300

    add: student entity

--- src/main/java/com/rocketseat/project_nwl/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/project_nwl/modules/students/entities/StudentEntity.java
@@ -3,6 +3,9 @@ package com.rocketseat.project_nwl.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,12 +13,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity(name = "students")
 public class StudentEntity {
     @Id
@@ -26,5 +31,6 @@ public class StudentEntity {
     private String email;
 
     @OneToMany(mappedBy = "studentEntity")
+    @JsonBackReference
     private List<CertificationStudentEntity> certificationStudentEntities;
 }
