5725315e522e64bd6af33e4e0ce4aef329f3267d
Brunowcst <brunoporfirio022@gmail.com>
2024-02-08 17:50:56 -0300

    add: update answer certification entity

--- src/main/java/com/rocketseat/project_nwl/modules/students/entities/AnswersCertificationsEntity.java
+++ src/main/java/com/rocketseat/project_nwl/modules/students/entities/AnswersCertificationsEntity.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,12 +15,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity(name = "answers_certification_students")
 public class AnswersCertificationsEntity {
     @Id
@@ -30,6 +34,7 @@ public class AnswersCertificationsEntity {
 
     @ManyToOne
     @JoinColumn(name = "certification_id", insertable = false, updatable = false)
+    @JsonBackReference
     private CertificationStudentEntity certificationStudentEntity;
 
     @Column(name = "student_id")
