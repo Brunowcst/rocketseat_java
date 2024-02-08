b4eee195db420037cedac26be5d67124e6ae1f0c
Brunowcst <brunoporfirio022@gmail.com>
2024-02-08 17:50:03 -0300

    add: student controller

--- src/main/java/com/rocketseat/project_nwl/modules/students/controller/StudentController.java
+++ src/main/java/com/rocketseat/project_nwl/modules/students/controller/StudentController.java
@@ -3,10 +3,13 @@ package com.rocketseat.project_nwl.modules.students.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rocketseat.project_nwl.modules.students.dto.StudentCertificationAnswerDTO;
 import com.rocketseat.project_nwl.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.project_nwl.modules.students.useCases.StudentCertificationAnswerUseCase;
 import com.rocketseat.project_nwl.modules.students.useCases.VerifyIsHasCertificationUseCase;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,6 +20,9 @@ public class StudentController {
 
     @Autowired
     private VerifyIsHasCertificationUseCase verifyCertificationUseCase;
+
+    @Autowired
+    private StudentCertificationAnswerUseCase studentCertificationAnswerUseCase;
     
     @PostMapping("/verifyIsHasCertification")
     public String verifyIsHasCertification(@RequestBody VerifyHasCertificationDTO
@@ -27,5 +33,15 @@ public class StudentController {
             return "Usuário já fez a prova";
         }
         return "Usuário pode fazer a prova";
+    }
+
+    @PostMapping("/certification/answer")
+    public ResponseEntity<Object> certificationAnswer(@RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) {
+        try {
+            var result = studentCertificationAnswerUseCase.execute(studentCertificationAnswerDTO);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
